71c97f480dd8e9d6ea8735a77ad760e9132fb04f
Shaqilaangelinaa <shaqila.angelina@gmail.com>
2025-05-21 13:41:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.title(" Selamat Datang Di Web Informatika ")
 st.write(
     " Ngodingseru bersama Pak Hendri")
+st.image("d58e0f7d-87be-445e-94db-bd21d75b25b4.jpeg", width=200)
